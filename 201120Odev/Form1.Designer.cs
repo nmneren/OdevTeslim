213a6c452e758bbda9129e7c58e214a1c7774812
nmneren <wew2ggs@gmail.com>
2020-11-22 19:20:25 +0300

    191120 verilen ödev

---
+++ 201120Odev/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace _201120Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_kayıtlı = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox_odenmis = new System.Windows.Forms.ListBox();
+            this.btn_ode = new System.Windows.Forms.Button();
+            this.txt_isim = new System.Windows.Forms.TextBox();
+            this.txt_tutar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_kaydet = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.btn_yenile = new System.Windows.Forms.Button();
+            this.btn_filtre = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txt_odenen_ara = new System.Windows.Forms.TextBox();
+            this.btn_gider_ara = new System.Windows.Forms.Button();
+            this.btn_sil_odenen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox_kayıtlı
+            // 
+            this.listBox_kayıtlı.FormattingEnabled = true;
+            this.listBox_kayıtlı.Location = new System.Drawing.Point(313, 61);
+            this.listBox_kayıtlı.Name = "listBox_kayıtlı";
+            this.listBox_kayıtlı.Size = new System.Drawing.Size(204, 251);
+            this.listBox_kayıtlı.TabIndex = 0;
+            this.listBox_kayıtlı.DoubleClick += new System.EventHandler(this.listBox_kayıtlı_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(341, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kayıtlı Ödemeler";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(589, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gider ve Ödenmiş Faturalar";
+            // 
+            // listBox_odenmis
+            // 
+            this.listBox_odenmis.FormattingEnabled = true;
+            this.listBox_odenmis.Location = new System.Drawing.Point(560, 111);
+            this.listBox_odenmis.Name = "listBox_odenmis";
+            this.listBox_odenmis.Size = new System.Drawing.Size(296, 199);
+            this.listBox_odenmis.TabIndex = 3;
+            // 
+            // btn_ode
+            // 
+            this.btn_ode.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_ode.Location = new System.Drawing.Point(189, 123);
+            this.btn_ode.Name = "btn_ode";
+            this.btn_ode.Size = new System.Drawing.Size(75, 38);
+            this.btn_ode.TabIndex = 4;
+            this.btn_ode.Text = "Öde";
+            this.btn_ode.UseVisualStyleBackColor = true;
+            this.btn_ode.Click += new System.EventHandler(this.btn_ode_Click);
+            // 
+            // txt_isim
+            // 
+            this.txt_isim.Location = new System.Drawing.Point(101, 62);
+            this.txt_isim.Name = "txt_isim";
+            this.txt_isim.Size = new System.Drawing.Size(163, 20);
+            this.txt_isim.TabIndex = 5;
+            this.txt_isim.TextChanged += new System.EventHandler(this.txt_isim_TextChanged);
+            // 
+            // txt_tutar
+            // 
+            this.txt_tutar.Location = new System.Drawing.Point(101, 88);
+            this.txt_tutar.Name = "txt_tutar";
+            this.txt_tutar.Size = new System.Drawing.Size(163, 20);
+            this.txt_tutar.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(16, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ödeme İsmi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(16, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ödeme Tutar";
+            // 
+            // btn_kaydet
+            // 
+            this.btn_kaydet.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_kaydet.Location = new System.Drawing.Point(101, 123);
+            this.btn_kaydet.Name = "btn_kaydet";
+            this.btn_kaydet.Size = new System.Drawing.Size(82, 38);
+            this.btn_kaydet.TabIndex = 9;
+            this.btn_kaydet.Text = "Kaydet";
+            this.btn_kaydet.UseVisualStyleBackColor = true;
+            this.btn_kaydet.Click += new System.EventHandler(this.btn_kaydet_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(117, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 22);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Gider Ödeme";
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_sil.Location = new System.Drawing.Point(426, 318);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(91, 38);
+            this.btn_sil.TabIndex = 11;
+            this.btn_sil.Text = "Sil";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // btn_yenile
+            // 
+            this.btn_yenile.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_yenile.Location = new System.Drawing.Point(101, 167);
+            this.btn_yenile.Name = "btn_yenile";
+            this.btn_yenile.Size = new System.Drawing.Size(163, 38);
+            this.btn_yenile.TabIndex = 12;
+            this.btn_yenile.Text = "Listeleri Yenile";
+            this.btn_yenile.UseVisualStyleBackColor = true;
+            this.btn_yenile.Click += new System.EventHandler(this.btn_yenile_Click);
+            // 
+            // btn_filtre
+            // 
+            this.btn_filtre.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_filtre.Location = new System.Drawing.Point(766, 59);
+            this.btn_filtre.Name = "btn_filtre";
+            this.btn_filtre.Size = new System.Drawing.Size(90, 22);
+            this.btn_filtre.TabIndex = 14;
+            this.btn_filtre.Text = "Filtrele";
+            this.btn_filtre.UseVisualStyleBackColor = true;
+            this.btn_filtre.Click += new System.EventHandler(this.btn_filtre_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(560, 59);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // txt_odenen_ara
+            // 
+            this.txt_odenen_ara.Location = new System.Drawing.Point(560, 85);
+            this.txt_odenen_ara.Name = "txt_odenen_ara";
+            this.txt_odenen_ara.Size = new System.Drawing.Size(200, 20);
+            this.txt_odenen_ara.TabIndex = 18;
+            this.txt_odenen_ara.Click += new System.EventHandler(this.txt_odenen_ara_Click);
+            this.txt_odenen_ara.TextChanged += new System.EventHandler(this.txt_odenen_ara_TextChanged);
+            // 
+            // btn_gider_ara
+            // 
+            this.btn_gider_ara.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_gider_ara.Location = new System.Drawing.Point(766, 85);
+            this.btn_gider_ara.Name = "btn_gider_ara";
+            this.btn_gider_ara.Size = new System.Drawing.Size(90, 22);
+            this.btn_gider_ara.TabIndex = 19;
+            this.btn_gider_ara.Text = "Ara";
+            this.btn_gider_ara.UseVisualStyleBackColor = true;
+            this.btn_gider_ara.Click += new System.EventHandler(this.btn_gider_ara_Click);
+            // 
+            // btn_sil_odenen
+            // 
+            this.btn_sil_odenen.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_sil_odenen.Location = new System.Drawing.Point(766, 318);
+            this.btn_sil_odenen.Name = "btn_sil_odenen";
+            this.btn_sil_odenen.Size = new System.Drawing.Size(91, 38);
+            this.btn_sil_odenen.TabIndex = 20;
+            this.btn_sil_odenen.Text = "Sil";
+            this.btn_sil_odenen.UseVisualStyleBackColor = true;
+            this.btn_sil_odenen.Click += new System.EventHandler(this.btn_sil_odenen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 423);
+            this.Controls.Add(this.btn_sil_odenen);
+            this.Controls.Add(this.btn_gider_ara);
+            this.Controls.Add(this.txt_odenen_ara);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btn_filtre);
+            this.Controls.Add(this.btn_yenile);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_kaydet);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_tutar);
+            this.Controls.Add(this.txt_isim);
+            this.Controls.Add(this.btn_ode);
+            this.Controls.Add(this.listBox_odenmis);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox_kayıtlı);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_kayıtlı;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox_odenmis;
+        private System.Windows.Forms.Button btn_ode;
+        private System.Windows.Forms.TextBox txt_isim;
+        private System.Windows.Forms.TextBox txt_tutar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_kaydet;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_yenile;
+        private System.Windows.Forms.Button btn_filtre;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txt_odenen_ara;
+        private System.Windows.Forms.Button btn_gider_ara;
+        private System.Windows.Forms.Button btn_sil_odenen;
+    }
+}
+
---
+++ 201120Odev/Form1.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _201120Odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\by_nu\Desktop\Gider";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            Yenile();
+
+        }
+
+        // Button Kısmı
+        private void btn_ode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ode(txt_isim.Text, Convert.ToDouble(txt_tutar.Text));
+
+                Yenile();
+
+                Temizle();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lütfen Boşlukları Doğru Biçimde Giriniz...");
+            }
+        }
+        private void btn_kaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Karsılastır(txt_isim.Text);
+
+                Yenile();
+
+                Temizle();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lütfen Boşlukları Doğru Biçimde Giriniz...");
+            }
+        }
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Sil(listBox_kayıtlı.SelectedIndex, 1);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private void btn_sil_odenen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Sil(listBox_odenmis.SelectedIndex, 2);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private void btn_yenile_Click(object sender, EventArgs e)
+        {
+            Yenile();
+        }
+        private void btn_filtre_Click(object sender, EventArgs e)
+        {
+            Ara(2);
+        }
+        private void btn_gider_ara_Click(object sender, EventArgs e)
+        {
+            Ara(1);
+        }
+
+        //Diğer Kısımlar
+        private void listBox_kayıtlı_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                txt_isim.Text = listBox_kayıtlı.SelectedItem.ToString();
+
+                btn_kaydet.Enabled = false;
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private void txt_isim_TextChanged(object sender, EventArgs e)
+        {
+            btn_kaydet.Enabled = true;
+        }
+        private void txt_odenen_ara_Click(object sender, EventArgs e) // burada arayama metnini textbox yazdıktan sonra silinmiyor tekrardan tıklandığında textbox içindeki metni otamatik seçiyor.
+        {
+            txt_odenen_ara.SelectAll();
+        }
+        private void txt_odenen_ara_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_odenen_ara.Text == "")
+            {
+                Yenile();
+            }
+        }
+
+        //Metot Kısmı
+        void Ode(string isim, double fiyat)
+        {
+            string path = @"C:\Users\by_nu\Desktop\Gider\odenen.txt";
+            string zaman = DateTime.Now.ToString().Split(' ')[0];
+
+            if (!File.Exists(path))
+            {
+                FileStream yaz = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(yaz);
+                sw.WriteLine(zaman + "-" + isim.ToUpper() + "-" + fiyat);
+                sw.Close();
+            }
+            else
+            {
+                FileStream yaz = new FileStream(path, FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(yaz);
+                sw.WriteLine(zaman + "-" + isim.ToUpper() + "-" + fiyat);
+                sw.Close();
+            }
+        }
+        void Kaydet(string isim)// burada kaydet yazılanı kaydetmek için
+        {
+            string path = @"C:\Users\by_nu\Desktop\Gider\kayitli.txt";
+
+            if (!File.Exists(path))
+            {
+                FileStream yaz = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(yaz);
+                sw.WriteLine(isim.ToUpper());
+                sw.Close();
+            }
+            else
+            {
+                FileStream yaz = new FileStream(path, FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(yaz);
+                sw.WriteLine(isim.ToUpper());
+                sw.Close();
+            }
+        }
+        void Sil(int index, int kontrol)
+        {
+            if (kontrol == 1)
+            {
+                string path = @"C:\Users\by_nu\Desktop\Gider\kayitli.txt";
+
+                if (index != -1)
+                {
+                    listBox_kayıtlı.Items.RemoveAt(index);
+
+                    File.Delete(path);
+
+                    string[] listekayitli = new string[listBox_kayıtlı.Items.Count];
+
+                    for (int i = 0; i < listBox_kayıtlı.Items.Count; i++)
+                    {
+                        listekayitli[i] = listBox_kayıtlı.Items[i].ToString();
+                    }
+
+                    Kaydet(listekayitli, 1);
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Seçim Yapınız...");
+                }
+            }
+            else if (kontrol == 2)
+            {
+                string path = @"C:\Users\by_nu\Desktop\Gider\odenen.txt";
+
+                if (index != -1)
+                {
+                    listBox_odenmis.Items.RemoveAt(index);
+
+                    File.Delete(path);
+
+                    string[] listeodenen = new string[listBox_odenmis.Items.Count];
+
+                    for (int i = 0; i < listBox_odenmis.Items.Count; i++)
+                    {
+                        listeodenen[i] = listBox_odenmis.Items[i].ToString();
+                    }
+
+                    Kaydet(listeodenen, 2);
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Seçim Yapınız...");
+                }
+            }
+
+
+        }
+        void Kaydet(string[] isim, int kontrol)//burada silindiğinde bir veri kalanları tekrardan kaydetmek için
+        {
+            if (kontrol == 1)
+            {
+                string path = @"C:\Users\by_nu\Desktop\Gider\kayitli.txt";
+
+                if (!File.Exists(path))
+                {
+                    FileStream yaz = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(yaz);
+
+                    foreach (var item in isim)
+                    {
+                        sw.WriteLine(item);
+                    }
+
+                    sw.Close();
+                }
+                else
+                {
+                    FileStream yaz = new FileStream(path, FileMode.Append, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(yaz);
+
+                    foreach (var item in isim)
+                    {
+                        sw.WriteLine(item);
+                    }
+
+                    sw.Close();
+                }
+            }
+            else if (kontrol == 2)
+            {
+                string path = @"C:\Users\by_nu\Desktop\Gider\odenen.txt";
+
+                if (!File.Exists(path))
+                {
+                    FileStream yaz = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(yaz);
+
+                    foreach (var item in isim)
+                    {
+                        sw.WriteLine(item);
+                    }
+
+                    sw.Close();
+                }
+                else
+                {
+                    FileStream yaz = new FileStream(path, FileMode.Append, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(yaz);
+
+                    foreach (var item in isim)
+                    {
+                        sw.WriteLine(item);
+                    }
+
+                    sw.Close();
+                }
+            }
+
+        }
+        void Karsılastır(string metin)
+        {
+            string path = @"C:\Users\by_nu\Desktop\Gider\kayitli.txt";
+
+            if (File.Exists(path))
+            {
+                string[] kayitliliste = File.ReadAllLines(path);
+
+                for (int i = 0; i < kayitliliste.Length; i++)
+                {
+                    if (!metin.Equals(kayitliliste[i], StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        Kaydet(metin);
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Zaten Var!");
+                        break;
+                    }
+                }
+            }
+        }
+        void Ara(int kontrol)
+        {
+            if (kontrol == 1)
+            {
+                if (txt_odenen_ara.Text != "")
+                {
+                    listBox_odenmis.Items.Clear();
+
+                    string path = @"C:\Users\by_nu\Desktop\Gider\odenen.txt";
+
+                    if (File.Exists(path))
+                    {
+                        string[] odenenliste = File.ReadAllLines(path);
+
+                        string[] yeniliste = new string[3];
+
+                        for (int i = 0; i < odenenliste.Length; i++)
+                        {
+                            yeniliste = odenenliste[i].Split('-');
+
+                            for (int j = 0; j < yeniliste.Length; j++)
+                            {
+                                if (txt_odenen_ara.Text.ToUpper().Contains(yeniliste[1]))
+                                {
+                                    listBox_odenmis.Items.Add(odenenliste[i]);
+                                    break;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Yenile();
+                }
+            }
+            else if (kontrol == 2)
+            {
+                listBox_odenmis.Items.Clear();
+
+                string path = @"C:\Users\by_nu\Desktop\Gider\odenen.txt";
+
+                if (File.Exists(path))
+                {
+                    string[] odenenliste = File.ReadAllLines(path);
+
+                    string[] yeniliste = new string[3];
+
+                    string[] zaman = dateTimePicker1.Value.ToString().Split(' ');
+
+
+                    for (int i = 0; i < odenenliste.Length; i++)
+                    {
+                        yeniliste = odenenliste[i].Split('-');
+
+                        for (int j = 0; j < yeniliste.Length; j++)
+                        {
+                            if (zaman[0].Contains(yeniliste[0]))
+                            {
+                                listBox_odenmis.Items.Add(odenenliste[i]);
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        void Yenile()
+        {
+            listBox_odenmis.Items.Clear();
+            listBox_kayıtlı.Items.Clear();
+            txt_odenen_ara.Clear();
+
+            string path = @"C:\Users\by_nu\Desktop\Gider\odenen.txt";
+            string[] listeOde = null;
+
+            if (File.Exists(path))
+            {
+                listeOde = File.ReadAllLines(path);
+
+                //tersden listelmesi için for döngüsü kullandım
+                for (int i = listeOde.Length; i > 0; i--)
+                {
+                    listBox_odenmis.Items.Add(listeOde[i - 1]);
+                }
+            }
+
+            string path2 = @"C:\Users\by_nu\Desktop\Gider\kayitli.txt";
+            string[] listekayitli = null;
+
+            if (File.Exists(path2))
+            {
+                listekayitli = File.ReadAllLines(path2);
+
+                foreach (var item in listekayitli)
+                {
+                    listBox_kayıtlı.Items.Add(item);
+                }
+            }
+        }
+        void Temizle()
+        {
+            txt_isim.Clear();
+            txt_tutar.Clear();
+        }
+
+    }
+}
